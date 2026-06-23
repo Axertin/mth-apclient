759b9e9df697bfae50fb4bd3ae1cac6b7832fe67
Axertin <oldsgolden@gmail.com>
2026-06-22 21:13:22 -0500

    fix(net): suppress kears unilaterally

--- src/mth/net/ap_link_apclient.cpp
+++ src/mth/net/ap_link_apclient.cpp
@@ -282,7 +282,7 @@ void ApLink::setup_handlers(const std::string &slot, const std::string &password
             auto missing = client_->get_missing_locations();
             auto checked = client_->get_checked_locations();
             const bool ossex_start = data.is_object() && data.value("ossex_start", 0) != 0;
-            const bool kear_rando = data.is_object() && data.value("kear_rando", 0) != 0;
+            const bool kear_rando = data.is_object(); // && data.value("kear_rando", 0) != 0;
             push_event(mth::ApConnected{client_->get_seed(), data.is_null() ? std::string{} : data.dump(), client_->get_player_number(),
                                         std::vector<std::int64_t>(checked.begin(), checked.end()), std::vector<std::int64_t>(missing.begin(), missing.end()),
                                         ossex_start, kear_rando});
