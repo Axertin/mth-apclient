7ffad114627ecee858309eb192e23e6be6da6451
Axertin <oldsgolden@gmail.com>
2026-07-03 16:16:57 -0500

    refactor: drop SessionPolicy env-modifier arming path

--- tests/unit/session_policy_test.cpp
+++ tests/unit/session_policy_test.cpp
@@ -9,19 +9,23 @@ TEST_CASE("nothing armed: enforcement follows AP authentication only", "[session
     REQUIRE(p.enforce_modifiers(true));
     REQUIRE_FALSE(p.enforce_caps(false));
     REQUIRE(p.enforce_caps(true));
+    REQUIRE_FALSE(p.enforce_abilities(false));
+    REQUIRE(p.enforce_abilities(true));
     REQUIRE_FALSE(p.caps_fixed());
 }
 
-TEST_CASE("env/console arming enforces modifiers without an AP session", "[session_policy]")
+TEST_CASE("console arming enforces per feature without an AP session", "[session_policy]")
 {
-    mth::SessionPolicy env;
-    env.arm_env_modifiers();
-    REQUIRE(env.enforce_modifiers(false));
+    mth::SessionPolicy mods;
+    mods.arm_console_modifiers();
+    REQUIRE(mods.enforce_modifiers(false));
+    REQUIRE_FALSE(mods.enforce_caps(false));      // modifiers arming does not arm caps
+    REQUIRE_FALSE(mods.enforce_abilities(false)); // nor abilities
 
-    mth::SessionPolicy console;
-    console.arm_console_modifiers();
-    REQUIRE(console.enforce_modifiers(false));
-    REQUIRE_FALSE(console.enforce_caps(false)); // modifiers arming does not arm caps
+    mth::SessionPolicy abil;
+    abil.arm_console_abilities();
+    REQUIRE(abil.enforce_abilities(false));
+    REQUIRE_FALSE(abil.enforce_modifiers(false));
 }
 
 TEST_CASE("forced caps enforce without AP and disable AP recompute", "[session_policy]")
